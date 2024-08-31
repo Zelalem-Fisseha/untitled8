d449ceb12c97281e86a214727c38140b1e0de71e
zelalem-fisseha <zelalem.fisseha@bitscollege.edu.et>
2024-08-31 19:53:16 +0300

    changes

--- src/components/cards.jsx
+++ src/components/cards.jsx
@@ -1,10 +1,8 @@
 
-
-// eslint-disable-next-line react/prop-types
 export function Cards({cardContents , ...props}) {
     return (
         <div className="cards">
-            {{cardContents}.map((item, index) => {
+            {cardContents.map((item, index) => {
                 return (
                     <div key={index } style={{...props.style}}>
                         <div> {item.title} </div>
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -3,13 +3,46 @@ import Sidebar from "../components/Sidebar.jsx";
 import Header from "../components/Header.jsx";
 function Dashboard() {
     return (
-        <div>
-              <Header/>
+        <div
+        style={{
+            display: "flex",
+            width: "100%",
+
+        }}>
+
+
             <Sidebar/>
+            <div
+                style={{width:"100%",
+                    display:"flex",
+                    flexDirection:"column",
+            }}>
+
+                <Header/>
+                <Cards cardContents={[{
+                    title: "home",
+                    description: "Home",
+                    count: 4
+                }]
 
 
-            {/*<Cards/>*/}
-        </div>
+                }/>
+
+
+            </div>
+
+
+
+
+
+
+
+            </div>
+
+
+
+
+
 
 
     )
